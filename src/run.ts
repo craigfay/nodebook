1ca72914bd4020021c966d4583f77adacfeb01e4
Craig Fay <craigfay1@gmail.com>
2019-09-06 17:27:02 -0500

    Escape package.json contents

--- src/run.ts
+++ src/run.ts
@@ -8,12 +8,33 @@ import { promisify } from 'util'
 const asyncExec = promisify(exec)
 
 export async function run(javascript:string) {
+  const packageJson = JSON.stringify({
+    "name": "nb",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    "keywords": [],
+    "author": "",
+    "license": "ISC",
+    "dependencies": {
+      "lodash": "^4.17.15"
+    }
+  });
+
   const dockerCommand = `
     docker run
+    --workdir=""
     node:12
-    node -e "${javascript}"
-  `.split('\n').join(' ')
+    bash -c "
+      printf '${packageJson.split('"').join('\\"')}' > package.json;
+      npm install;
+    "
+ `.split('\n').join(' ')
 
   const { stdout } = await asyncExec(dockerCommand)
   return stdout;
 }
+
+// echo ${packageJson} > package.json 
+// && npm install
+// && node -e "${javascript}"
