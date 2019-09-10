1db25f3350366ed3d862af301100f3d326cf4676
Craig Fay <craigfay1@gmail.com>
2019-09-10 18:14:46 -0500

    add package cell

--- src/notebook.ts
+++ src/notebook.ts
@@ -6,6 +6,15 @@ function push(notebook, newCell) {
   return  { cells: [...cells, newCell], ...rest }
 }
 
+
+
+function packageCell(content) {
+  return {
+    cellType: 'package',
+    content,
+  }
+}
+
 function codeCell(content) {
   return {
     cellType: 'code',
@@ -20,11 +29,11 @@ function textCell(content) {
   }
 }
 
-function run(notebook) {
-  const codeCells = notebook.cells.filter(cell => cell.cellType == 'code');
-  const contents = codeCells.map(cell => cell.content);
-  eval(contents.join('\n'));
-}
+// function run(notebook) {
+  // const codeCells = notebook.cells.filter(cell => cell.cellType == 'code')
+  // const contents = codeCells.map(cell => cell.content)
+  // eval(contents.join('\n' + 'console.log("*end_cell*")' + '\n'))
+// }
 
 const instance = build(
   notebook => push(notebook, codeCell('console.log(5)')),
