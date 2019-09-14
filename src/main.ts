6a145cf8c9122a4d3af9cf06cf1d688c7afa9ce6
Craig Fay <craigfay1@gmail.com>
2019-09-14 15:44:37 -0500

    Move generic static routes below specific

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { run } from './run'
 import * as notebook from './notebook'
 
 const s = http.server();
+s.route('GET', '/codemirror/*', staticFiles(__dirname + "/node_modules"));
 s.route('GET', '/*.html', staticFiles(__dirname + "/static"));
 s.route('GET', '/*.css', staticFiles(__dirname + "/static"));
-s.route('GET', '/codemirror/*', staticFiles(__dirname + "/node_modules"));
 s.route('POST', '/api/run', async (req, meta) => {
   const dependencies = notebook.getDependencies(req.body)
   const javascript = notebook.getExecutable(req.body)
